0821f83d9dc49ee5baeeb988c795e08d3bd22236
Jurek11 <yura.abc@gmail.com>
2012-05-02 21:41:53 +0200

    Unit test second

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -10,4 +10,22 @@ test "product attributes must not be empty" do
 	assert product.errors[:image_url].any?
 end
 
+test "product price must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", 
+      product.errors[:price].join('; ')
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01", 
+      product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
 end
