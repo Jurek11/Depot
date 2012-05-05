0fa478104aa8f7633da4242e3454a356e07dd89b
Jurek11 <yura.abc@gmail.com>
2012-05-05 12:52:35 +0200

    Pt-D-1-2.Add session to template

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	@count = increment_count
   end
+  
+  def increment_count
+  	if session[:counter].nil?
+  		session[:counter]=0
+  	end
+  	session[:counter] +=1
+  end
+
 end
